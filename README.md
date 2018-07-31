f1ff28642843f90058fc6da3c5188ee9ca43215d
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2018-07-31 14:16:33 +0200

    update angular app

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NttTime
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.1.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'time-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'time';
