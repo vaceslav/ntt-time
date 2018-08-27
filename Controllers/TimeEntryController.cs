6107c693bb2a97245fae753f27f20cda418c41af
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2018-08-27 23:28:16 +0200

    start with time entries

---
+++ Controllers/TimeEntryController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NttTimeApi.Db;
+
+namespace ntt_time.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeEntryController : ControllerBase
+    {
+
+        private readonly NttDbContext _context;
+
+         public TimeEntryController(NttDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        [ProducesResponseType( typeof( List<TimeEntry> ), 200 )]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.TimeEntries.ToListAsync());
+        }
+
+        [HttpPost("")]
+        [ProducesResponseType( typeof(TimeEntry), 200 )]
+        public async Task<IActionResult> Create([FromBody] TimeEntry entry)
+        {
+            var result = await _context.TimeEntries.AddAsync(entry);
+            _context.SaveChanges();
+            return Ok(entry);
+        }
+
+    }
+}
---
+++ Migrations/20180827212051_AddEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntttime.Migrations
+{
+    public partial class AddEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimeEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeEntries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeEntries");
+        }
+    }
+}
