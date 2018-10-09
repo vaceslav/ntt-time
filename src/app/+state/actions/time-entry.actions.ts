2ff70e568af03b14bbeb6f2941e3cf67f09b9700
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2018-10-09 15:11:18 +0200

    change to interface

--- src/app/+state/actions/time-entry.actions.ts
+++ src/app/+state/actions/time-entry.actions.ts
@@ -1,4 +1,4 @@
-import { TimeEntry } from '../../shared/swagger';
+import { ITimeEntry } from '../../shared/swagger';
 
 export const LOAD_TIME_ENTRIES = '[TIME_ENTRY] LOAD_TIME_ENTRIES';
 export const LOAD_TIME_ENTRIES_SUCCESS = '[TIME_ENTRY] LOAD_TIME_ENTRIES_SUCCESS';
@@ -11,17 +11,17 @@ export class LoadTimeEntries {
 
 export class LoadTimeEntriesSuccess {
   readonly type = LOAD_TIME_ENTRIES_SUCCESS;
-  constructor(public payload: TimeEntry[]) {}
+  constructor(public payload: ITimeEntry[]) {}
 }
 
 export class CreateTimeEntry {
   readonly type = CREATE_TIME_ENTRY;
-  constructor(public payload: TimeEntry) {}
+  constructor(public payload: ITimeEntry) {}
 }
 
 export class CreateTimeEntrySuccess {
   readonly type = CREATE_TIME_ENTRY_SUCCESS;
-  constructor(public payload: TimeEntry) {}
+  constructor(public payload: ITimeEntry) {}
 }
 
 export type TimeEntryAction = LoadTimeEntries | LoadTimeEntriesSuccess | CreateTimeEntry | CreateTimeEntrySuccess;
