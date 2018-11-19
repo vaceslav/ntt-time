910574240a2170c269afff6a3b3842a0f79ec92a
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2018-11-19 17:16:22 +0100

    update to v7

--- src/app/+state/effects/time-entry.effects.ts
+++ src/app/+state/effects/time-entry.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
@@ -10,44 +10,42 @@ import * as timeActions from '../actions/time-entry.actions';
 @Injectable()
 export class TimeEntryEffects {
   @Effect()
-  loadAll$: Observable<Action> = this.actions$.ofType<timeActions.LoadTimeEntries>(timeActions.LOAD_TIME_ENTRIES).pipe(
+  loadAll$: Observable<Action> = this.actions$.pipe(
+    ofType<timeActions.LoadTimeEntries>(timeActions.LOAD_TIME_ENTRIES),
     switchMap(action => {
       return this.timeEntryClient.get().pipe(map(items => new timeActions.LoadTimeEntriesSuccess(items)));
     })
   );
 
   @Effect()
-  createItem$: Observable<Action> = this.actions$
-    .ofType<timeActions.CreateTimeEntry>(timeActions.CREATE_TIME_ENTRY)
-    .pipe(
-      switchMap(action => {
-        return this.timeEntryClient
-          .create(action.payload as TimeEntry)
-          .pipe(map(item => new timeActions.CreateTimeEntrySuccess(item)));
-      })
-    );
+  createItem$: Observable<Action> = this.actions$.pipe(
+    ofType<timeActions.CreateTimeEntry>(timeActions.CREATE_TIME_ENTRY),
+    switchMap(action => {
+      return this.timeEntryClient
+        .create(action.payload as TimeEntry)
+        .pipe(map(item => new timeActions.CreateTimeEntrySuccess(item)));
+    })
+  );
 
   @Effect()
-  updateItem$: Observable<Action> = this.actions$
-    .ofType<timeActions.UpdateTimeEntry>(timeActions.UPDATE_TIME_ENTRY)
-    .pipe(
-      switchMap(action => {
-        return this.timeEntryClient
-          .update(action.payload.id, action.payload as TimeEntry)
-          .pipe(map(item => new timeActions.UpdateTimeEntrySuccess(item)));
-      })
-    );
+  updateItem$: Observable<Action> = this.actions$.pipe(
+    ofType<timeActions.UpdateTimeEntry>(timeActions.UPDATE_TIME_ENTRY),
+    switchMap(action => {
+      return this.timeEntryClient
+        .update(action.payload.id, action.payload as TimeEntry)
+        .pipe(map(item => new timeActions.UpdateTimeEntrySuccess(item)));
+    })
+  );
 
   @Effect()
-  deleteItem$: Observable<Action> = this.actions$
-    .ofType<timeActions.DeleteTimeEntry>(timeActions.DELETE_TIME_ENTRY)
-    .pipe(
-      switchMap(action => {
-        return this.timeEntryClient
-          .delete(action.payload.id)
-          .pipe(map(item => new timeActions.DeleteTimeEntrySuccess(action.payload)));
-      })
-    );
+  deleteItem$: Observable<Action> = this.actions$.pipe(
+    ofType<timeActions.DeleteTimeEntry>(timeActions.DELETE_TIME_ENTRY),
+    switchMap(action => {
+      return this.timeEntryClient
+        .delete(action.payload.id)
+        .pipe(map(item => new timeActions.DeleteTimeEntrySuccess(action.payload)));
+    })
+  );
 
   constructor(private actions$: Actions, private timeEntryClient: TimeEntryClient) {}
 }
