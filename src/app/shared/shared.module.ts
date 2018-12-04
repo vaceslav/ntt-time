e69e8a28767cad3dcc2e31729f243da51b759907
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2018-12-04 18:04:29 +0100

    live

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,22 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatIconModule } from '@angular/material';
 import { EntryDetailComponent } from 'src/app/shared/components/entry-detail/entry-detail.component';
+import { AmazingTimePickerModule } from 'amazing-time-picker';
 
 import { EntryComponent } from './components/entry/entry.component';
+import { HourPipe } from './pipes/hour.pipe';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, FormsModule, MatButtonModule, MatIconModule, FlexLayoutModule],
-  declarations: [EntryComponent, EntryDetailComponent],
-  exports: [EntryComponent, EntryDetailComponent]
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    FlexLayoutModule,
+    AmazingTimePickerModule
+  ],
+  declarations: [EntryComponent, EntryDetailComponent, HourPipe],
+  exports: [EntryComponent, EntryDetailComponent, HourPipe]
 })
 export class SharedModule {}
