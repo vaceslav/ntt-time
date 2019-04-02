956f5da90dcef16d6d443565ff1b472d0300023c
Slava <slava@xeqo.de>
2019-04-02 08:38:32 +0200

    ffdds

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatIconModule, MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatDatepickerModule, MatIconModule, MatInputModule } from '@angular/material';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { EntryDetailComponent } from 'src/app/shared/components/entry-detail/entry-detail.component';
 
@@ -15,9 +19,12 @@ import { HourPipe } from './services/hours.pipe';
     CommonModule,
     HttpClientModule,
     FormsModule,
+    MatFormFieldModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
     FlexLayoutModule,
     NgxMaterialTimepickerModule.forRoot()
   ],
