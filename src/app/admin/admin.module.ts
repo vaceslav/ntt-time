64e01e71c965d23cabede55f2ac42ebb89aa55a5
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2020-02-08 13:04:17 +0100

    update material

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
